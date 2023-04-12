e67619625076f3014f9ffb0f6904962ae76169be
Andreï V. Kostyrka <fifis.himik@gmail.com>
2023-04-12 19:20:53 +0200

    Version bump

--- R/StatecCMP/R/zzz.R
+++ R/StatecCMP/R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname) {
-	packageStartupMessage("Statec functions and wrappers, version 0.0.6.2, 2023-02-28.")
+	packageStartupMessage("Statec functions and wrappers, version 0.0.6.3, 2023-04-12.")
   packageStartupMessage("Maintained by Michel Geller (michel.geller@statec.etat.lu),\npreviously by Andrei V. Kostyrka (andrei.kostyrka@gmail.com).")
 }
